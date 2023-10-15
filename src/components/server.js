3d56e67b202361d822c5d9c33c5dd5f8aff6989d
vivaldev <valtteri.virtanen.dev@gmail.com>
2023-10-15 12:43:55 +0300

    server.js login mock-up

--- src/components/server.js
+++ src/components/server.js
@@ -1,3 +1,4 @@
+// Basic server
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -10,4 +11,18 @@ app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log("The server is running at port:5000.");
+});
+
+// Login mock-up
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Authentication logic here later on
+    // Now it's hardcoded to be 'user' and 'password'
+    if (username == 'user' && password == 'password') {
+        res.json({ success: true, message: 'Login succesful!' });
+    } else {
+        res.status(401).json({ success: false, message: 'Invalid credentials. '});
+    }
 });
